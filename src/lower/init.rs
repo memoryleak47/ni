efb9ed4ab81f1c4ddd0c14a6a2c0be5be9b71959
memoryleak47 <memoryleak47@protonmail.ch>
2025-05-23 16:24:50 +0200

    impl integer addition as __add__

--- src/lower/init.rs
+++ src/lower/init.rs
@@ -64,6 +64,29 @@ fn add_singletons(ctxt: &mut Ctxt) {
     add_primitive_type("float");
     add_primitive_type("bool");
     add_primitive_type("NoneType");
+
+    // TODO: move somewhere else: This just implements integer addition.
+    let fid = new_fn(ctxt, |ctxt| {
+        let arg = ctxt.push_arg();
+        let singleton = ctxt.push_index_str(arg, "singletons");
+        let int_ty = ctxt.push_index_str(singleton, "int");
+        let a = ctxt.push_index_int(arg, 0);
+        let a = ctxt.push_index_str(a, "payload");
+
+        let b = ctxt.push_index_int(arg, 1);
+        let b = ctxt.push_index_str(b, "payload");
+
+        let v = ctxt.push_compute(Expr::BinOp(BinOpKind::Plus, a, b));
+        let v = ctxt.build_value(v, int_ty);
+        ctxt.push_store_str(arg, "ret", v);
+        ctxt.push_return();
+    });
+    let int_ty = ctxt.get_singleton("int");
+    let fn_ty = ctxt.get_singleton("function");
+    let int_dict = ctxt.push_index_str(int_ty, "dict");
+    let f = ctxt.push_compute(Expr::Function(fid));
+    let f_val = ctxt.build_value(f, fn_ty);
+    ctxt.push_store_str(int_dict, "__add__", f_val);
 }
 
 pub fn add_builtins_and_singletons(ctxt: &mut Ctxt) {
