1679a009c29a8d94923b2739947e9b91738b466f
memoryleak47 <memoryleak47@protonmail.ch>
2025-05-28 18:12:22 +0200

    add basic (single inheritance) MRO construction

--- src/lower/init.rs
+++ src/lower/init.rs
@@ -55,10 +55,74 @@ fn add_singletons(ctxt: &mut Ctxt) {
         ctxt.push_store_str(singleton, name, tab);
     }
 
+    // add MRO to builtin types.
+    // object MRO:
+    let obj = ctxt.get_singleton("object");
+    let obj_mro_list = ctxt.push_table();
+    let one = ctxt.push_int(1);
+    ctxt.push_store_str(obj_mro_list, "len", one);
+    ctxt.push_store_int(obj_mro_list, 0, obj);
+    let list_ty = ctxt.get_singleton("list");
+    let obj_mro = ctxt.build_value(obj_mro_list, list_ty);
+    ctxt.push_store_str(obj, "mro", obj_mro);
+    // other MRO:
+    for name in PRIM_TYPES {
+        if *name != "object" {
+            add_mro(ctxt.get_singleton(name), &[], ctxt);
+        }
+    }
+
     let s = ctxt.push_str("None"); // hack: for now 'None' contains a string for correct printing.
     let none_ty = ctxt.get_singleton("NoneType");
     let none = ctxt.build_value(s, none_ty);
     ctxt.push_store_str(ctxt.f().singletons_node, "None", none);
+}
+
+// for now, mro computation just accepts a single base type.
+// we just prepend ty to bases[0].mro
+fn add_mro(ty: Node, bases: &[Node], ctxt: &mut Ctxt) {
+    let base = match bases[..] {
+        [base] => base,
+        [] => ctxt.get_singleton("object"),
+        _ => todo!("multiple inheritance not supported yet"),
+    };
+    let mro_list = ctxt.push_table();
+    let base_mro = ctxt.push_index_str(base, "mro");
+    let base_mro_list = ctxt.push_index_str(base_mro, "payload");
+    let base_len = ctxt.push_index_str(base_mro_list, "len");
+    let one = ctxt.push_int(1);
+    let new_len = ctxt.push_compute(Expr::BinOp(BinOpKind::Plus, base_len, one));
+    ctxt.push_store_str(mro_list, "len", new_len);
+    ctxt.push_store_int(mro_list, 0, ty);
+
+    let pre_loop = ctxt.alloc_blk();
+    let in_loop = ctxt.alloc_blk();
+    let post_loop = ctxt.alloc_blk();
+
+    // for i in 0..base_len: mro_list[i+1] = base_mro_list[i]
+    let i = ctxt.push_table();
+    let zero = ctxt.push_int(0);
+    ctxt.push_store_str(i, "0", zero);
+    ctxt.push_goto(pre_loop);
+
+    ctxt.focus_blk(pre_loop);
+        let a = ctxt.push_index_str(i, "0");
+        let b = base_len;
+        let cond = ctxt.push_compute(Expr::BinOp(BinOpKind::Lt, a, b));
+        ctxt.push_if(cond, in_loop, post_loop);
+
+    ctxt.focus_blk(in_loop);
+        let i_val = ctxt.push_index_str(i, "0");
+        let i1_val = ctxt.push_compute(Expr::BinOp(BinOpKind::Plus, i_val, one));
+        let idx = ctxt.push_index(base_mro_list, i_val);
+        ctxt.push_store(mro_list, i1_val, idx); // mro_list[i+1] = base_mro_list[i]
+        ctxt.push_store_str(i, "0", i1_val); // i = i+1
+        ctxt.push_goto(pre_loop);
+
+    ctxt.focus_blk(post_loop);
+        let list_ty = ctxt.get_singleton("list");
+        let mro = ctxt.build_value(mro_list, list_ty);
+        ctxt.push_store_str(ty, "mro", mro);
 }
 
 static PRIM_TYPES: &[&'static str] = &[
@@ -70,6 +134,7 @@ static PRIM_TYPES: &[&'static str] = &[
     "bool",
     "list",
     "NoneType",
+    "object",
 ];
 
 static OPS: &[BinOpKind] =
