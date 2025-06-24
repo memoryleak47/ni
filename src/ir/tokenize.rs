c53cb233797128798ca8ad64b5b7b379b70fdf0c
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-24 16:10:42 +0200

    impl IRtokenize

--- src/ir/tokenize.rs
+++ src/ir/tokenize.rs
@@ -2,35 +2,19 @@ use crate::*;
 
 #[derive(Debug, PartialEq)]
 pub enum IRToken {
-    Ident(String),
+    Symbol(Symbol),
     Int(i64),
     Str(String),
     Bool(bool),
-    None,
-    Colon,
-    LParen,
-    RParen,
-    LBracket,
-    RBracket,
-    Comma,
-    Dot,
-    Equals,
-    If,
-    While,
-    Return,
-    Break,
-    Continue,
-    Def,
-    Class,
-    Pass,
-    Scope(ScopeKind),
-    Newline,
-    Indent,
-    Unindent,
+    Undef,
+
+    LParen, RParen,
+    LBracket, RBracket,
+    LBrace, RBrace,
+    Comma, Dot, Equals,
+
+    Let, Proc, Exit, Jmp,
     BinOp(BinOpKind),
-    Try,
-    Except,
-    Raise
 }
 
 fn ident_char(c: char) -> bool {
@@ -42,93 +26,60 @@ fn int_char(c: char) -> bool {
 }
 
 enum TokenizerState {
-    CountingIndents(usize),
-    InLine,
+    None,
     InInt(String),
     InStr(char, String),
     InIdent(String),
     InComment, // #
 }
 
-pub fn ir_tokenize(s: &str) -> Vec<Token> {
+pub fn ir_tokenize(s: &str) -> Vec<IRToken> {
     let mut chars: Vec<_> = s.chars().collect();
     chars.push('#'); // automatically closes off final idents.
-
-    // tells how deeply indented previous indents were.
-    // this stack is never empty!
-    let mut indent_stack: Vec<usize> = vec![0];
 
     let mut i = 0;
     let mut tokens = Vec::new();
 
-    let mut state = TokenizerState::CountingIndents(0);
+    let mut state = TokenizerState::None;
 
     while i < chars.len() {
         let c = chars[i];
         match state {
-            TokenizerState::CountingIndents(n) => {
-                if c == '\t' {
-                    state = TokenizerState::CountingIndents(((n + 8)/8)*8);
-                    i += 1;
-                } else if c == ' ' {
-                    state = TokenizerState::CountingIndents(n + 1);
-                    i += 1;
-                } else if c == '\n' {
-                    // this ignores empty lines.
-                    state = TokenizerState::CountingIndents(0);
-                    i += 1;
-                } else {
-                    state = TokenizerState::InLine;
-                    if n > *indent_stack.last().unwrap() {
-                        tokens.push(Token::Indent);
-                        indent_stack.push(n);
-                    }
-                    while n < *indent_stack.last().unwrap() {
-                        indent_stack.pop();
-                        tokens.push(Token::Unindent);
-                    }
-                }
-            }
-            TokenizerState::InLine => match &chars[i..] {
-                ['+', ..] => { tokens.push(Token::BinOp(BinOpKind::Plus)); i += 1; }
-                ['-', ..] => { tokens.push(Token::BinOp(BinOpKind::Minus)); i += 1; }
-                ['*', '*', ..] => { tokens.push(Token::BinOp(BinOpKind::Pow)); i += 2; }
-                ['*', ..] => { tokens.push(Token::BinOp(BinOpKind::Mul)); i += 1; }
-                ['/', ..] => { tokens.push(Token::BinOp(BinOpKind::Div)); i += 1; }
-                ['%', ..] => { tokens.push(Token::BinOp(BinOpKind::Mod)); i += 1; }
-                ['<', '=', ..] => { tokens.push(Token::BinOp(BinOpKind::Le)); i += 2; }
-                ['>', '=', ..] => { tokens.push(Token::BinOp(BinOpKind::Ge)); i += 2; }
-                ['=', '=', ..] => { tokens.push(Token::BinOp(BinOpKind::IsEqual)); i += 2; }
-                ['!', '=', ..] => { tokens.push(Token::BinOp(BinOpKind::IsNotEqual)); i += 2; }
-                ['<', ..] => { tokens.push(Token::BinOp(BinOpKind::Lt)); i += 1; }
-                ['>', ..] => { tokens.push(Token::BinOp(BinOpKind::Gt)); i += 1; }
+            TokenizerState::None => match &chars[i..] {
+                ['+', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Plus)); i += 1; }
+                ['-', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Minus)); i += 1; }
+                ['*', '*', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Pow)); i += 2; }
+                ['*', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Mul)); i += 1; }
+                ['/', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Div)); i += 1; }
+                ['%', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Mod)); i += 1; }
+                ['<', '=', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Le)); i += 2; }
+                ['>', '=', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Ge)); i += 2; }
+                ['=', '=', ..] => { tokens.push(IRToken::BinOp(BinOpKind::IsEqual)); i += 2; }
+                ['!', '=', ..] => { tokens.push(IRToken::BinOp(BinOpKind::IsNotEqual)); i += 2; }
+                ['<', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Lt)); i += 1; }
+                ['>', ..] => { tokens.push(IRToken::BinOp(BinOpKind::Gt)); i += 1; }
                 ['"', ..] => { state = TokenizerState::InStr('"', String::new()); i += 1; }
                 ['\'', ..] => { state = TokenizerState::InStr('\'', String::new()); i += 1; }
-                ['\n', ..] => { state = TokenizerState::CountingIndents(0); i += 1; }
                 ['#', ..] => { state = TokenizerState::InComment; i += 1; }
-                [':', ..] => { tokens.push(Token::Colon); i += 1; }
-                ['(', ..] => { tokens.push(Token::LParen); i += 1; }
-                [')', ..] => { tokens.push(Token::RParen); i += 1; }
-                ['[', ..] => { tokens.push(Token::LBracket); i += 1; }
-                [']', ..] => { tokens.push(Token::RBracket); i += 1; }
-                [',', ..] => { tokens.push(Token::Comma); i += 1; }
-                ['=', ..] => { tokens.push(Token::Equals); i += 1; }
-                ['.', ..] => { tokens.push(Token::Dot); i += 1; }
+                ['(', ..] => { tokens.push(IRToken::LParen); i += 1; }
+                [')', ..] => { tokens.push(IRToken::RParen); i += 1; }
+                ['[', ..] => { tokens.push(IRToken::LBracket); i += 1; }
+                [']', ..] => { tokens.push(IRToken::RBracket); i += 1; }
+                ['{', ..] => { tokens.push(IRToken::LBrace); i += 1; }
+                ['}', ..] => { tokens.push(IRToken::RBrace); i += 1; }
+                [',', ..] => { tokens.push(IRToken::Comma); i += 1; }
+                ['=', ..] => { tokens.push(IRToken::Equals); i += 1; }
+                ['.', ..] => { tokens.push(IRToken::Dot); i += 1; }
 
-                [' ', ..] => { i += 1; } _ if int_char(c) => {
-                    state = TokenizerState::InInt(c.to_string());
-                    i += 1;
-                }
-                _ if ident_char(c) => {
-                    state = TokenizerState::InIdent(c.to_string());
-                    i += 1;
-                }
+                _ if int_char(c) => { state = TokenizerState::InInt(c.to_string()); i += 1; },
+                _ if ident_char(c) => { state = TokenizerState::InIdent(c.to_string()); i += 1; },
+                _ if c.is_whitespace() => { i += 1; },
                 _ => panic!("unknown char '{c}'"),
             },
             TokenizerState::InStr(delim, mut s) => {
                 if c == delim {
-                    tokens.push(Token::Str(s));
-                    state = TokenizerState::InLine;
+                    tokens.push(IRToken::Str(s));
+                    state = TokenizerState::None;
                 } else {
                     s.push(c);
                     state = TokenizerState::InStr(delim, s);
@@ -142,25 +93,13 @@ pub fn ir_tokenize(s: &str) -> Vec<Token> {
                     i += 1;
                 } else {
                     tokens.push(match &*s {
-                        "if" => Token::If,
-                        "while" => Token::While,
-                        "return" => Token::Return,
-                        "break" => Token::Break,
-                        "continue" => Token::Continue,
-                        "def" => Token::Def,
-                        "class" => Token::Class,
-                        "pass" => Token::Pass,
-                        "True" => Token::Bool(true),
-                        "False" => Token::Bool(false),
-                        "None" => Token::None,
-                        "global" => Token::Scope(ScopeKind::Global),
-                        "nonlocal" => Token::Scope(ScopeKind::NonLocal),
-                        "try" => Token::Try,
-                        "except" => Token::Except,
-                        "raise" => Token::Raise,
-                        _ => Token::Ident(s),
+                        "let" => IRToken::Let,
+                        "proc" => IRToken::Proc,
+                        "exit" => IRToken::Exit,
+                        "jmp" => IRToken::Jmp,
+                        _ => IRToken::Symbol(gsymb_add(s)),
                     });
-                    state = TokenizerState::InLine;
+                    state = TokenizerState::None;
                 }
             }
             TokenizerState::InInt(mut s) => {
@@ -170,17 +109,15 @@ pub fn ir_tokenize(s: &str) -> Vec<Token> {
                     i += 1;
                 } else {
                     let int = s.parse().expect("Can't parse int!");
-                    tokens.push(Token::Int(int));
-                    state = TokenizerState::InLine;
+                    tokens.push(IRToken::Int(int));
+                    state = TokenizerState::None;
                 }
             }
             TokenizerState::InComment => {
                 if c == '\n' {
-                    state = TokenizerState::CountingIndents(0);
-                    i += 1;
-                } else {
-                    i += 1;
+                    state = TokenizerState::None;
                 }
+                i += 1;
             }
         }
     }
