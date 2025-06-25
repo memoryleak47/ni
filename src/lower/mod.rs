1b2f1063f2dabb725a827dd2d9df9829119a7972
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-25 09:48:21 +0200

    clear previous lowering

--- src/lower/mod.rs
+++ src/lower/mod.rs
@@ -1,26 +1,11 @@
 use crate::*;
 
-/*
-mod init;
-pub use init::*;
-
 mod nameres;
 pub use nameres::*;
 
-mod ctxt;
-pub use ctxt::*;
-
-mod fl;
-pub use fl::*;
-
-mod expr;
-pub use expr::*;
-
-mod stmt;
-pub use stmt::*;
-*/
-
 fn lower_ast(ast: &AST) -> String {
+    let nameres_tab = nameres(ast);
+
     String::new()
 }
 
@@ -31,44 +16,3 @@ pub fn lower(ast: &AST) -> IR {
     let toks = ir_tokenize(&s);
     ir_assemble(&toks[..])
 }
-
-/*
-pub fn lower(ast: &AST) -> IR {
-    let nameres_tab = nameres(ast);
-
-    let mut ctxt = Ctxt {
-        stack: Vec::new(),
-        nameres_tab,
-        ir: IR { main_fn: 0, fns: Default::default() },
-        builtin_fns: Default::default(),
-    };
-
-    let main = new_fn_general(true, &mut ctxt, |ctxt| {
-        let t = ctxt.push_table();
-        ctxt.f_mut().lowering = Some(FnLowerCtxt {
-            // for the main function, the global scope is actually it's local scope.
-            global_node: t,
-            namespace_node: t,
-            ast_ptr: 0 as _,
-            loop_stack: Vec::new(),
-        });
-
-
-        add_builtins_and_singletons(ctxt);
-        lower_ast(ast, ctxt);
-
-        ctxt.push_return();
-    });
-
-    ctxt.ir.main_fn = main;
-    ctxt.ir
-}
-
-pub fn find_namespace(v: &str, ctxt: &mut Ctxt) -> Node {
-    let k = (ctxt.fl().ast_ptr, v.to_string());
-    match ctxt.nameres_tab.get(&k) {
-        Some(VarPlace::Local) => ctxt.fl().namespace_node,
-        _ => ctxt.fl().global_node,
-    }
-}
-*/
