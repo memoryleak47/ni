34f8d79432438a24954e1cced946a29a3e7a85c8
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-25 13:17:56 +0200

    ...

--- src/lower/mod.rs
+++ src/lower/mod.rs
@@ -19,21 +19,21 @@ fn lower_ast(ast: &AST) -> String {
     for stmt in &**ast {
         s.push_str(&*lower_stmt(stmt));
     }
-    s.push_str("    exit;\n");
+    // s.push_str("    exit;\n");
     s.push_str("}\n");
     s
 }
 
 fn lower_stmt(stmt: &ASTStatement) -> String {
     match stmt {
-        ASTStatement::Expr(e) => lower_expr(e),
+        ASTStatement::Expr(e) => format!("    {};\n", lower_expr(e)),
         _ => todo!(),
     }
 }
 
 fn lower_expr(e: &ASTExpr) -> String {
     match e {
-        ASTExpr::FnCall(f, args) => format!("jmp @.globals[{}].pid", lower_expr(f)),
+        ASTExpr::FnCall(f, args) => format!("jmp {}.pid", lower_expr(f)),
         ASTExpr::Var(v) => format!("@.globals[\"{v}\"]"),
         _ => todo!(),
     }
