8e9e3485468dc9e25ae57a2306ebf3436411ebaa
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-26 10:42:51 +0200

    revamp fmt system

--- src/ir/fmt.rs
+++ src/ir/fmt.rs
@@ -1,10 +1,6 @@
 use crate::*;
 use std::fmt::{self, Display, Formatter};
 
-const SHOW_ALL: bool = false;
-
-type NodeMap = Map<Node, (String, /*is-symbol: */ bool)>;
-
 impl Display for IR {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         for (&pid, _) in self.procs.iter() {
@@ -21,34 +17,31 @@ pub fn display_proc(pid: Symbol, ir: &IR, f: &mut Formatter<'_>) -> fmt::Result 
     write!(f, "{main_prefix}proc {pid} {{\n")?;
 
     let proc = &ir.procs[&pid];
-    let mut nodemap = Map::new();
-    for st in &proc.stmts {
-        display_stmt(st, &mut nodemap, f)?;
+    for i in 0..proc.stmts.len() {
+        display_stmt(i, proc, f)?;
     }
-    display_terminator(&ir.procs[&pid].terminator, &nodemap, f)?;
+    display_terminator(proc, f)?;
     write!(f, "}}\n\n")
 }
 
-fn display_stmt(stmt: &Statement, nodemap: &mut NodeMap, f: &mut Formatter<'_>) -> fmt::Result {
+fn display_stmt(stmt_id: usize, proc: &Proc, f: &mut Formatter<'_>) -> fmt::Result {
     use Statement::*;
 
+    let stmt = &proc.stmts[stmt_id];
     match stmt {
         Let(n, e, visible) => {
-            let e_str = expr_string(e, nodemap);
-            if *visible || SHOW_ALL {
+            let e_str = expr_string(e, proc);
+            if *visible {
                 write!(f, "    {n} = {e_str};\n")?;
-            } else {
-                let b = matches!(e, Expr::Symbol(_));
-                nodemap.insert(*n, (e_str, b));
             }
         }
         Store(t, i, n) => {
-            let idx = display_index(*t, *i, nodemap);
-            let n = node_string(*n, nodemap);
+            let idx = display_index(*t, *i, proc);
+            let n = node_string(*n, proc);
             write!(f, "    {idx} = {n};\n")?;
         }
         Print(v) => {
-            let v = node_string(*v, nodemap);
+            let v = node_string(*v, proc);
             write!(f, "    print {v};\n")?;
         },
     }
@@ -56,17 +49,18 @@ fn display_stmt(stmt: &Statement, nodemap: &mut NodeMap, f: &mut Formatter<'_>) 
     Ok(())
 }
 
-fn display_terminator(terminator: &Terminator, nodemap: &NodeMap, f: &mut Formatter<'_>) -> fmt::Result {
+fn display_terminator(proc: &Proc, f: &mut Formatter<'_>) -> fmt::Result {
     use Terminator::*;
 
+    let terminator = &proc.terminator;
     match terminator {
         Jmp(n) => {
-            let n = node_string(*n, nodemap);
+            let n = node_string(*n, proc);
             write!(f, "    jmp {n};\n")?;
         },
         Exit => write!(f, "    exit;\n")?,
         Panic(n) => {
-            let n = node_string(*n, nodemap);
+            let n = node_string(*n, proc);
             write!(f, "    panic {n};\n")?;
         },
     }
@@ -74,20 +68,20 @@ fn display_terminator(terminator: &Terminator, nodemap: &NodeMap, f: &mut Format
     Ok(())
 }
 
-fn expr_string(expr: &Expr, nodemap: &NodeMap) -> String {
+fn expr_string(expr: &Expr, proc: &Proc) -> String {
     use Expr::*;
     match expr {
-        Index(t, i) => display_index(*t, *i, nodemap),
+        Index(t, i) => display_index(*t, *i, proc),
         Root => format!("@"),
         NewTable => format!("{{}}"),
         BinOp(kind, l, r) => {
-            let l = node_string(*l, nodemap);
-            let r = node_string(*r, nodemap);
+            let l = node_string(*l, proc);
+            let r = node_string(*r, proc);
             format!("{l} {kind} {r}")
         }
         Index(l, r) => {
-            let l = node_string(*l, nodemap);
-            let r = node_string(*r, nodemap);
+            let l = node_string(*l, proc);
+            let r = node_string(*r, proc);
             format!("{l}[{r}]")
         }
         Symbol(s) => format!("{s}"),
@@ -121,10 +115,12 @@ impl Display for BinOpKind {
     }
 }
 
-fn node_string(n: Node, nodemap: &NodeMap) -> String {
-    match nodemap.get(&n) {
-        Some((x, _)) => x.to_string(),
-        None => n.to_string(),
+fn node_string(n: Node, proc: &Proc) -> String {
+    let (expr, b) = get_def(n, proc);
+    if b {
+        expr_string(expr, proc)
+    } else {
+        format!("%{n}")
     }
 }
 
@@ -134,10 +130,20 @@ impl Display for Node {
     }
 }
 
-fn display_index(t: Node, i: Node, nodemap: &NodeMap) -> String {
-    let b = nodemap.get(&i).map(|(_, x)| *x).unwrap_or(false);
-    let t = node_string(t, nodemap);
-    let i = node_string(i, nodemap);
+fn get_def(n: Node, proc: &Proc) -> (&Expr, bool) {
+    for x in proc.stmts.iter() {
+        if let Statement::Let(n2, expr, b) = x && n == *n2 {
+            return (expr, *b);
+        }
+    }
+    unreachable!()
+}
+
+fn display_index(t: Node, i: Node, proc: &Proc) -> String {
+    let (e, _) = get_def(i, proc);
+    let b = matches!(e, Expr::Symbol(_));
+    let t = node_string(t, proc);
+    let i = node_string(i, proc);
 
     if b {
         format!("{t}.{i}")
