68d51a745494ffa16ff3d9156bd092f91a4e3d9b
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-27 15:40:30 +0200

    ...

--- src/ir/exec.rs
+++ src/ir/exec.rs
@@ -202,7 +202,8 @@ fn step_stmt(stmt: &Statement, ctxt: &mut Ctxt) -> bool {
 
 // returns "false" when done.
 fn step(ctxt: &mut Ctxt) -> bool {
-    let proc = &ctxt.ir.procs[&ctxt.pid];
+    let pid = ctxt.pid;
+    let proc = &ctxt.ir.procs.get(&pid).unwrap_or_else(|| panic!("Can't run unknown proc symbol '{pid}'"));
     let stmt = &proc.stmts.get(ctxt.statement_idx).unwrap();
     ctxt.last_stmt = StmtFmt { stmt_id: ctxt.statement_idx, proc }.to_string();
 
