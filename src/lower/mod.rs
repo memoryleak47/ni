faba49973f24ee7c38f5b970a6a09e4782bffde2
memoryleak47 <memoryleak47@protonmail.ch>
2025-06-29 11:56:21 +0200

    bugfix

--- src/lower/mod.rs
+++ src/lower/mod.rs
@@ -224,11 +224,11 @@ fn lower_body(stmts: &[ASTStatement], ctxt: &mut Ctxt) {
                 ctxt.focus_blk(suc);
             },
             ASTStatement::Raise(body) => {
-                let h = ctxt.alloc_irlocal("handler");
-                ctxt.push(format!("{h} = @.handler"));
-                ctxt.push(format!("@.frame = {h}.frame"));
-                ctxt.push(format!("@.handler = {h}.parent"));
-                ctxt.push(format!("jmp {h}.pid"));
+                let h = Symbol::new_fresh("handler");
+                ctxt.push(format!("%{h} = @.handler"));
+                ctxt.push(format!("@.frame = %{h}.frame"));
+                ctxt.push(format!("@.handler = %{h}.parent"));
+                ctxt.push(format!("jmp %{h}.pid"));
             }
             _ => todo!(),
         }
