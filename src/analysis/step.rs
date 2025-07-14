37f6dbdc3ea1366b6f7442478f9f9bba510015f2
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-14 10:42:27 +0200

    ~analysis: step_stmt

--- src/analysis/step.rs
+++ src/analysis/step.rs
@@ -4,7 +4,11 @@ impl AnalysisState {
     pub fn step(&mut self, i: SpecId) {
         let states = {
             let spec = &self.specs[&i];
-            let mut states = vec![spec.st.clone()];
+            let fst = FullThreadState {
+                st: spec.st.clone(),
+                nodes: Map::new(),
+            };
+            let mut states = vec![fst];
             for stmt in self.ir.procs[&spec.st.pid].stmts.iter() {
                 let mut new_states = Vec::new();
                 for st in states {
@@ -15,12 +19,12 @@ impl AnalysisState {
             states
         };
 
-        for st in states {
+        for fst in states {
             let id: SpecId = Id(self.specs.len());
             self.queue.push_back(id);
 
             let spec = Spec {
-                st,
+                st: fst.st,
                 outs: Default::default(),
             };
             self.specs.insert(id, spec);
@@ -28,6 +32,40 @@ impl AnalysisState {
     }
 }
 
-fn step_stmt(st: ThreadState, stmt: &Statement) -> Vec<ThreadState> {
-    todo!()
+struct FullThreadState {
+    st: ThreadState,
+    nodes: Map<Node, ValueId>,
+}
+
+fn step_expr(st: FullThreadState, expr: &Expr) -> Vec<(ValueId, FullThreadState)> {
+    match expr {
+        Expr::Index(_, _) => todo!(),
+        Expr::Root => todo!(),
+        Expr::NewTable => todo!(),
+        Expr::BinOp(_, _, _) => todo!(),
+        Expr::Input => todo!(),
+
+        Expr::Symbol(_) => todo!(),
+        Expr::Float(_) => todo!(),
+        Expr::Int(_) => todo!(),
+        Expr::Str(_) => todo!(),
+    }
+}
+
+fn step_stmt(st: FullThreadState, stmt: &Statement) -> Vec<FullThreadState> {
+    match stmt {
+        Statement::Let(n, expr, _) => {
+            // let val = step_expr(expr, ctxt);
+            // ctxt.nodes.insert(*n, val);
+            todo!()
+        }
+        Statement::Store(t, idx, n) => {
+            todo!()
+        }
+        Statement::Jmp(n) => {
+            todo!()
+        }
+        Statement::Print(_) => vec![st],
+        Statement::Exit | Statement::Panic(_) | Statement::Fail => Vec::new(),
+    }
 }
