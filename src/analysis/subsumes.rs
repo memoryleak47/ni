1ce950538fc20fbf9ab8540d53b59ddca61a4e09
memoryleak47 <memoryleak47@protonmail.ch>
2025-09-16 15:54:10 +0200

    impl heap cache

--- src/analysis/subsumes.rs
+++ src/analysis/subsumes.rs
@@ -28,11 +28,13 @@ pub fn subsumes(general: &ThreadState, special: &ThreadState) -> bool {
     // Incase tids_general is empty, we want some place to map everything to.
     tids_general.insert(TableSortId(Symbol::new_fresh("DEFAULT_TID".to_string())));
 
+    let cache = heap_cache(&general);
+
     let phi = tids_special.iter().map(|x|
             if ASSUME_IDENTITY && tids_general.contains(x) { (*x, std::iter::once(*x).collect()) }
             else { (*x, tids_general.clone()) }
         ).collect();
-    let out = solve_constraints(phi, &constraints, &general).is_some();
+    let out = solve_constraints(phi, &constraints, &general, &cache).is_some();
     out
 }
 
@@ -54,20 +56,20 @@ fn build_constraints(special: &ThreadState) -> Vec<Constraint> {
     constraints
 }
 
-fn solve_constraints(mut phi: Phi, constraints: &[Constraint], general: &ThreadState) -> Option<Homomorphism> {
+fn solve_constraints(mut phi: Phi, constraints: &[Constraint], general: &ThreadState, cache: &HeapCache) -> Option<Homomorphism> {
     assert!(phi.iter().all(|(_, y)| y.len() > 0));
-    if !constraints_satisfiable(&phi, constraints, general) { return None; }
+    if !constraints_satisfiable(&phi, constraints, general, cache) { return None; }
 
     if let Some((x, y)) = phi.iter().find(|(_, y)| y.len() > 1).map(|(x, y)| (x.clone(), y.clone())) {
         let mut phi_new = phi.clone();
         let y1 = y.iter().next().unwrap().clone();
         phi_new.insert(x, std::iter::once(y1).collect());
-        if let Some(hom) = solve_constraints(phi_new, constraints, general) {
+        if let Some(hom) = solve_constraints(phi_new, constraints, general, cache) {
             return Some(hom);
         }
         phi[&x].swap_remove(&y1);
 
-        solve_constraints(phi, constraints, general)
+        solve_constraints(phi, constraints, general, cache)
     } else {
         let hom: Homomorphism = phi.iter().map(|(x, y)| {
             assert!(y.len() == 1);
@@ -86,12 +88,12 @@ fn eval_phi(p: &ValueParticle, phi: &Phi) -> ValueSet {
     }
 }
 
-fn constraints_satisfiable(phi: &Phi, constraints: &[Constraint], general: &ThreadState) -> bool {
+fn constraints_satisfiable(phi: &Phi, constraints: &[Constraint], general: &ThreadState, cache: &HeapCache) -> bool {
     for Constraint { a, t, k } in constraints {
         let t = eval_phi(t, phi);
         let k = eval_phi(k, phi);
         let a = eval_phi(a, phi);
-        let v = index(&t, &k, general);
+        let v = index(&t, &k, general, cache);
         let cond = a.0.iter().any(|a| {
             a.subseteq(&v, &general.deref)
         });
@@ -101,11 +103,13 @@ fn constraints_satisfiable(phi: &Phi, constraints: &[Constraint], general: &Thre
     true
 }
 
-fn index(t: &ValueSet, k: &ValueSet, st: &ThreadState) -> ValueSet {
+fn index(t: &ValueSet, k: &ValueSet, st: &ThreadState, cache: &HeapCache) -> ValueSet {
     let mut vs = ValueSet::bottom();
     for t in &t.0 {
         for k in &k.0 {
-            vs = vs.union(&index_p(t, k, st), &st.deref);
+            if let Some(v) = cache.get(&[t.clone(), k.clone()]) {
+                vs = vs.union(v, &st.deref);
+            }
         }
     }
     vs
@@ -130,4 +134,25 @@ fn tids(st: &ThreadState) -> Set<TableSortId> {
         }
     }
     set
+}
+
+type HeapCache = Map<[ValueParticle; 2], ValueSet>;
+
+fn heap_cache(st: &ThreadState) -> HeapCache {
+    let mut map = Map::new();
+    for e in st.table_entries.iter() {
+        let TableEntry::Add(t, k, v) = e else { continue };
+        for t in &t.0 {
+            let mut k_list: Vec<ValueParticle> = k.0.clone();
+            while let Some(k) = k_list.pop() {
+                let vv: &mut ValueSet = map.entry([t.clone(), k.clone()]).or_insert_with(|| ValueSet(Vec::new()));
+                *vv = vv.union(v, &st.deref);
+                if let Some(k_upc) = upcast(&k, &st.deref) {
+                    k_list.extend(k_upc.0);
+                }
+            }
+        }
+    }
+
+    map
 }
