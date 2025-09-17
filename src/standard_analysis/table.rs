d5c4a7219809c4910104bad12c192156afdb6a4e
memoryleak47 <memoryleak47@protonmail.ch>
2025-09-17 16:12:20 +0200

    standard_analysis: impl upcasting in tab_store

--- src/standard_analysis/table.rs
+++ src/standard_analysis/table.rs
@@ -13,21 +13,27 @@ pub fn tab_index(t: &ValueSet, k: &ValueSet, st: &ProcState) -> ValueSet {
     vs.compactify()
 }
 
-// TODO: upcast "k".
 pub fn tab_store(t: &ValueSet, k: &ValueSet, v: &ValueSet, st: &mut ProcState) {
     let concrete = t.is_concrete() && k.is_concrete();
     for t in &t.0 {
-        assert!(matches!(t, ValueParticle::Concrete(_)|ValueParticle::Summary(_)));
-
+        if !matches!(t, ValueParticle::Concrete(_)|ValueParticle::Summary(_)) { continue }
         for k in &k.0 {
-            let tk = [t.clone(), k.clone()];
-            let vv = st.tables.entry(tk).or_default();
-            if concrete {
-                *vv = v.clone();
-            } else {
-                let undef_v = ValueSet(vec![ValueParticle::Symbol(Symbol::new("Undef"))]);
-                *vv = vv.union(v).union(&undef_v);
+            store_impl(t, k, v, concrete, st);
+            if let ValueParticle::String(_) = k {
+                store_impl(t, &ValueParticle::TopString, v, false, st);
+            } else if let ValueParticle::Int(_) = k {
+                store_impl(t, &ValueParticle::TopInt, v, false, st);
             }
         }
+    }
+}
+
+fn store_impl(t: &ValueParticle, k: &ValueParticle, v: &ValueSet, concrete: bool, st: &mut ProcState) {
+    let tk = [t.clone(), k.clone()];
+    let vv = st.tables.entry(tk).or_insert_with(|| ValueSet(vec![ValueParticle::Symbol(Symbol::new("Undef"))]));
+    if concrete {
+        *vv = v.clone();
+    } else {
+        *vv = vv.union(v);
     }
 }
