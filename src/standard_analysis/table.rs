02d854cc1fddc68dfc393d1017a3331ceb6722ca
memoryleak47 <memoryleak47@protonmail.ch>
2025-09-17 14:29:59 +0200

    standard_analysis: add Undef table stuff

--- src/standard_analysis/table.rs
+++ src/standard_analysis/table.rs
@@ -4,17 +4,27 @@ pub fn tab_index(t: &ValueSet, k: &ValueSet, st: &ProcState) -> ValueSet {
     let mut vs = ValueSet::bottom();
     for t in &t.0 {
         for k in &k.0 {
-            vs.0.extend(st.tables[&[t.clone(), k.clone()]].0.clone());
+            let tk = [t.clone(), k.clone()];
+            let undef_v = ValueSet(vec![ValueParticle::Symbol(Symbol::new("Undef"))]);
+            let vv = st.tables.get(&tk).cloned().unwrap_or(undef_v);
+            vs.0.extend(vv.0);
         }
     }
     vs.compactify()
 }
 
 pub fn tab_store(t: &ValueSet, k: &ValueSet, v: &ValueSet, st: &mut ProcState) {
+    let concrete = t.is_concrete() && k.is_concrete();
     for t in &t.0 {
         for k in &k.0 {
-            let vv = &mut st.tables[&[t.clone(), k.clone()]];
-            *vv = vv.union(v);
+            let tk = [t.clone(), k.clone()];
+            let vv = st.tables.entry(tk).or_default();
+            if concrete {
+                *vv = v.clone();
+            } else {
+                let undef_v = ValueSet(vec![ValueParticle::Symbol(Symbol::new("Undef"))]);
+                *vv = vv.union(v).union(&undef_v);
+            }
         }
     }
 }
