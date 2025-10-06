1fd8c8a51997ce03e5ffbd0bc786f9ac366f6434
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-06 15:29:54 +0200

    finish list(range(..)) impl

--- etests/57.py
+++ etests/57.py
@@ -6,4 +6,10 @@ l = [2, 43, 6]
 l.append(5)
 print(l[1] + l[3])
 
-# TODO add more list shenanigans
+l = list(range(2, 5))
+l.append(5)
+print(l[1] + l[3])
+
+l = list(range(5))
+l.append(5)
+print(l[1] + l[3])
