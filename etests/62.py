05e6ed3b4e45ebbf42df1f4352858671a8e531e9
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-13 17:18:05 +0200

    finish basic list.insert impl

---
+++ etests/62.py
@@ -0,0 +1,15 @@
+# testing list.insert
+
+l = [3]
+l.insert(0, 0)
+print(l[0])
+print(l[1])
+l.insert(2, 2)
+print(l[0])
+print(l[1])
+print(l[2])
+l.insert(1, 7)
+print(l[0])
+print(l[1])
+print(l[2])
+print(l[3])
