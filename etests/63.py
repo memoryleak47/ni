a24c4f9626c2c7a83d021a64c9b67aeaa53500be
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-23 18:03:37 +0200

    stupid bugfix

--- etests/63.py
+++ etests/63.py
@@ -1,9 +1,11 @@
 l = list(range(100))[1:21:4]
-for i in range(5):
+print(l.__len__())
+for i in range(l.__len__()):
     print(l[i])
 
 print("----")
 l[0:5:2] = [222, 333, 444]
 
-for i in range(5):
+print(l.__len__())
+for i in range(l.__len__()):
     print(l[i])
--- etests/64.py
+++ etests/64.py
@@ -1,7 +1,9 @@
 l = [1, 2, 3][::1]
+print(l.__len__())
 for i in range(3):
     print(l[i])
 
 l = [1, 2, 3][::-1]
+print(l.__len__())
 for i in range(3):
     print(l[i])
